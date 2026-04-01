64f09bc2d31e4391448ee57b2fb6c25a97828960
dank-star <dk19914@gmail.com>
2026-04-01 09:17:49 -0500

    Create Parcel3.java

---
+++ Parcel3.java
@@ -0,0 +1,36 @@
+//: Parcel3.java
+// Returning a handle to an inner class
+package c07.parcel3;
+
+abstract class Contents {
+  abstract public int value();
+}
+
+interface Destination {
+  String readLabel();
+}
+
+public class Parcel3 {
+  private class PContents extends Contents {
+    private int i = 11;
+    public int value() { return i; }
+  }
+  protected class PDestination
+    implements Destination {
+      private String label;
+      private Pdestination(String whereTo) {
+        label = whereTo;
+      }
+      public String readLabel() { return label; }
+    }
+  public Destination dest(String s) {
+    return new PDestination(s);
+  }
+  public Contents cont() {
+    return new PContents();
+  }
+}
+
+class Test {
+  public static void main(String[] args){
+    Parcel3 p = new Parcel3();
